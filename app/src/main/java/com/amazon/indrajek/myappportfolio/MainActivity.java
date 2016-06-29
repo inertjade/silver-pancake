e39d633dacb508d9d36e8c4327b0d7e9d85feb95
Indrajeet Kalyankar <indrajek@amazon.com>
2016-06-29 12:08:00 -0500

    Added toasts

--- app/src/main/java/com/amazon/indrajek/myappportfolio/MainActivity.java
+++ app/src/main/java/com/amazon/indrajek/myappportfolio/MainActivity.java
@@ -1,8 +1,11 @@
 package com.amazon.indrajek.myappportfolio;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,52 +13,64 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Context context = getApplicationContext();
+        final int duration = Toast.LENGTH_SHORT;
+
+        final Button buildItBiggerButton = (Button) findViewById(R.id.buildItBiggerButton);
+        buildItBiggerButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                displayToast(buildItBiggerButton.getText().toString().toLowerCase(), context, duration);
+            }
+        });
+
+        final Button popularMoviesButton = (Button) findViewById(R.id.popularMoviesButton);
+        popularMoviesButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                displayToast(popularMoviesButton.getText().toString().toLowerCase(), context, duration);
+            }
+        });
+
+        final Button capstoneButton = (Button) findViewById(R.id.capstoneButton);
+        capstoneButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                displayToast(capstoneButton.getText().toString().toLowerCase(), context, duration);
+            }
+        });
+
+        final Button goUbitquitousButton = (Button) findViewById(R.id.goUbitquitousButton);
+        goUbitquitousButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                displayToast(goUbitquitousButton.getText().toString().toLowerCase(), context, duration);
+            }
+        });
+
+        final Button makeYourAppMaterialButton = (Button) findViewById(R.id.makeYourAppMaterialButton);
+        makeYourAppMaterialButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                displayToast(makeYourAppMaterialButton.getText().toString().toLowerCase(), context, duration);
+            }
+        });
+
+        final Button StockHawkButton = (Button) findViewById(R.id.StockHawkButton);
+        StockHawkButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                displayToast(StockHawkButton.getText().toString().toLowerCase(), context, duration);
+            }
+        });
+
+
     }
 
-    Button button = (Button) findViewById(R.id.button_send);
-    button.setOnClickListener(new View.OnClickListener() {
-        public void onClick(View v) {
-            // Do something in response to button click
-        }
-    });
-
-    Button button = (Button) findViewById(R.id.button_send);
-    button.setOnClickListener(new View.OnClickListener() {
-        public void onClick(View v) {
-            // Do something in response to button click
-        }
-    });
-
-    Button button = (Button) findViewById(R.id.button_send);
-    button.setOnClickListener(new View.OnClickListener() {
-        public void onClick(View v) {
-            // Do something in response to button click
-        }
-    });
-
-    Button button = (Button) findViewById(R.id.button_send);
-    button.setOnClickListener(new View.OnClickListener() {
-        public void onClick(View v) {
-            // Do something in response to button click
-        }
-    });
-
-    Button button = (Button) findViewById(R.id.button_send);
-    button.setOnClickListener(new View.OnClickListener() {
-        public void onClick(View v) {
-            // Do something in response to button click
-        }
-    });
-
-    Button button = (Button) findViewById(R.id.button_send);
-    button.setOnClickListener(new View.OnClickListener() {
-        public void onClick(View v) {
-            // Do something in response to button click
-        }
-    });
-
-    /** Called when the user touches the button */
-    public void displayToast(View view) {
-        // Do something in response to button click
+    public void displayToast(CharSequence text, Context context, int duration) {
+        Toast toast = Toast.makeText(context, "This button will launch my " + text + " app!", duration);
+        toast.show();
     }
+
 }
