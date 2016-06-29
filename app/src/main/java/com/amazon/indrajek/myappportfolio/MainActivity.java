1c996381a05c9313058d00d7d61fcd7fa244d298
Indrajeet Kalyankar <indrajek@amazon.com>
2016-06-29 11:17:15 -0500

    First Commit

---
+++ app/src/main/java/com/amazon/indrajek/myappportfolio/MainActivity.java
@@ -0,0 +1,61 @@
+package com.amazon.indrajek.myappportfolio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    Button button = (Button) findViewById(R.id.button_send);
+    button.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+            // Do something in response to button click
+        }
+    });
+
+    Button button = (Button) findViewById(R.id.button_send);
+    button.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+            // Do something in response to button click
+        }
+    });
+
+    Button button = (Button) findViewById(R.id.button_send);
+    button.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+            // Do something in response to button click
+        }
+    });
+
+    Button button = (Button) findViewById(R.id.button_send);
+    button.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+            // Do something in response to button click
+        }
+    });
+
+    Button button = (Button) findViewById(R.id.button_send);
+    button.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+            // Do something in response to button click
+        }
+    });
+
+    Button button = (Button) findViewById(R.id.button_send);
+    button.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+            // Do something in response to button click
+        }
+    });
+
+    /** Called when the user touches the button */
+    public void displayToast(View view) {
+        // Do something in response to button click
+    }
+}
